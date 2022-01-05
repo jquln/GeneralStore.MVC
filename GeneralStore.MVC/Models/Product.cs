dec3b29db5d41eb4a5551de40dee447593923031
Jacquelyn Mitchell <jac.nmitchell@gmail.com>
2022-01-05 12:38:53 -0500

    Add project files.

---
+++ GeneralStore.MVC/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public int InventoryCount { get; set; }
+
+        [Required]
+        [Display(Name = "It is Food")]
+        public bool IsFood { get; set; }
+    }
+}
